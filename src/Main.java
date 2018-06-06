026a8c637c2e279380e5e0c0741db19ad7de32a7
Mima <mima28412@gmail.com>
2018-06-06 11:11:20 +0200

    Novi projekat Vezba2, klasa Main, klasa Student

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+import student.Student;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Student Milica = new Student("Milica", "Subotic", 1993);
+        Milica.info();
+    }
+    
+}
